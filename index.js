a4c090bf28040bacb06d3b5631e8dd2ec7f55392
Josh Milo <josh@joshmilo.com>
2015-08-06 21:33:30 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var bob = "bob";
+function findName(name) {
+    return name;
+}
+var phil = "phil";
+var karl = "karl";
+var boo = "boo";
---
+++ index.ts
@@ -0,0 +1,9 @@
+/* awesome */
+let bob: string = "bob";
+
+function findName(name: string): string {
+  return name;
+}
+let phil: string = "phil";
+let karl = "karl";
+let boo = "boo";
